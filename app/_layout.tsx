11c66a856f0d281b8777e463cd168eb61b9dee48
raghu-raj23 <raghuraj.muni@gmail.com>
2026-03-29 23:55:19 +0530

    simplified the app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,19 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-import '@/global.css'
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import "@/global.css";
+import { Stack } from "expo-router";
+import "react-native-reanimated";
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+	anchor: "(tabs)",
 };
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+	return (
+		<Stack>
+			<Stack.Screen name="index" options={{ headerShown: false }} />
+			<Stack.Screen
+				name="modal"
+				options={{ presentation: "modal", title: "Modal" }}
+			/>
+		</Stack>
+	);
 }
